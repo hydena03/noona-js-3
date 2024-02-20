e40434d058a3dda027eb660e29ebb4d2c631a1bd
KYEONGROK KIM <hydena03@gamil.com>
2024-02-20 22:12:42 +0900

    second commit

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 const API_KEY = `e8b9019be4404f7a9760c1636a0c451c`;
 let news=[]
 const getLatestNews = async ()=> {
-  const url = new URL(
-    `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
-    );
+  const url = new URL(`https://noona-api-practice.netlify.app/`);
+
   const response = await fetch(url)
   const data = await response.json();
-  news = data.articles
-  console.log("ddd", data.articles);
+  news = data.articles;
+  console.log("dddd", news);
 };
 
 getLatestNews();
