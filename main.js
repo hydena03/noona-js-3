7ae1513c1442b8c803d04a2ec5f71c73496712e1
KYEONGROK KIM <hydena03@gamil.com>
2024-02-20 18:16:54 +0900

    second commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY = `e8b9019be4404f7a9760c1636a0c451c`;
 let news=[]
 const getLatestNews = async ()=> {
   const url = new URL(
-    `https://noona-api-practice.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`
+    `https://noona-api-practice.netlify.app/top-headlines?`
     );
   const response = await fetch(url)
   const data = await response.json();
