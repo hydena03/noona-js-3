b4ed9ae89bda9b5bd5098b071278a5292be115b1
KYEONGROK KIM <hydena03@gamil.com>
2024-02-21 17:39:37 +0900

    second commit

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
+const API_KEY = `e8b9019be4404f7a9760c1636a0c451c`;
+let newsList=[]
 
-let news=[]
 const getLatestNews = async ()=> {
   const url = new URL(`https://noona-api-practice.netlify.app/top-headlines?country=kr`);
 
+// const getLatestNews = async ()=> {
+//      const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apikey=${API_KEY}`);
+
   const response = await fetch(url)
   const data = await response.json();
-  news = data.articles;
-  console.log("dddd", news);
+  newsList = data.articles;
+  render();
+  console.log("dddd", newsList);
 };
+
+const render = ()=>{
+  const newsHTML = newsList.map(news=>`<div class="row news">
+  <div class="col-lg-4">
+  <img class="news-img-size" src=${news.urlToImage}>
+  </div>
+    <div class="col-lg-8">
+  <h2>${news.title}</h2>
+  <p>${news.description}</p>
+  <div>${news.source.name}*${news.publishedAt}</div>
+  </div>
+  </div>`).join(``);
+
+
+  document.getElementById("news-board").innerHTML=newsHTML
+}
 
 getLatestNews();
 
