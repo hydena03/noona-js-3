6453ffc0f0ac493bb4ce929e5d3223376cc58dbd
KYEONGROK KIM <hydena03@gamil.com>
2024-02-29 17:55:08 +0900

    first commit

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const menus = document.querySelectorAll(".menus button")
 menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)));
 
 let url = new URL(`https://noona-api-practice.netlify.app/top-headlines?country=kr&apikey=${API_KEY}`)
-// let url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apikey=${API_KEY}`);
+// let url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=business&apikey=${API_KEY}`);
 
 let totalResults = 0;
 let page = 1;
@@ -68,29 +68,26 @@ const searchNews = async () => {
 const render = () => {
   const newsHTML = newsList.map(news => `<div class="row news">
   <div class="col-lg-4">
-  <img class="news-img-size"
-  src="${news.urlToImage ||
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"}"/>
-
-  
+    <a href="${news.url}" target="_blank">
+      <img class="news-img-size" src="${news.urlToImage ||
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"}"/>
+    </a>
   </div>
-    <div class="col-lg-8">
-  <h2>${news.title}</h2>
-  <p>${news.description == null || news.description == ""
-      ? "내용없음"
-      : news.description.length > 200
-        ? news.description.substring(0, 200) + "..."
-        : news.description
-    }</p>
-<div>${news.source.name || "no source"}  ${moment(
+  <div class=" news col-lg-8">
+    <h2><a href="${news.url}" target="_blank">${news.title}</a></h2>
+    <p>${news.description == null || news.description == ""
+        ? "내용없음"
+        : news.description.length > 200
+          ? news.description.substring(0, 200) + "..."
+          : news.description
+      }</p>
+    <div>${news.source.name || "no source"}  ${moment(
       news.publishedAt
     ).format('llll')}</div>
-
   </div>
-  </div>`).join(``);
+</div>`).join(``);
 
-
-  document.getElementById("news-board").innerHTML = newsHTML
+  document.getElementById("news-board").innerHTML = newsHTML;
 };
 
 const errorRender = (errorMessage) => {
